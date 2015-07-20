b7131f686dd3cf9d36d093e63543cc22a68abb02
HENRY.M <henry.m@mahuan.info>
2015-07-20 16:02:41 +0800

    add baidu tongji plugin

---
+++ js/baidu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?a68951ed2be55445b59b52701e8f14d3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
